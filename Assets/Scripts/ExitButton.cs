5cb3f96bdd7fc87cacf715ef403714e006d17158
maxyodedara5 <maxyodedara5@gmail.com>
2017-08-21 23:20:48 +0530

    2D UFO
    Added the whole game to the repository

---
+++ Assets/Scripts/ExitButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ExitButton : MonoBehaviour
+{
+    public Button yourButton;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Application.Quit();
+        Debug.Log("You have clicked the button!");
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    private int count;
+    private Rigidbody2D rb2d;
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private void Start()
+    {
+        count = 0;
+        rb2d = GetComponent<Rigidbody2D>();
+        winText.text = "";
+        SetCountText(); 
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed); 
+
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+        
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 11)
+            winText.text = "You Win";
+    }
+
+
+}
